00590e540e8e525122cf8735dc111798d38d7347
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-09-25 17:02:23 +0200

    Update pwmixer_test.c

--- src/pwmixer_test/pwmixer_test.c
+++ src/pwmixer_test/pwmixer_test.c
@@ -25,10 +25,10 @@ int main(int argc, char *argv[]) {
 
 	pwm_sysConnect(&argc, &argv);
 
-	pwm_IO *in = pwm_ioCreateInput("To Out + Out2", false);
-	pwm_IO *in2 = pwm_ioCreateInput("To Out2 only", true);
-	pwm_IO *out = pwm_ioCreateOutput("Out", true);
-	pwm_IO *out2 = pwm_ioCreateOutput("Out2", false);
+	pwm_IO *in = pwm_ioCreateInput("To Out + Out2", true);
+	pwm_IO *in2 = pwm_ioCreateInput("To Out2 only", false);
+	pwm_IO *out = pwm_ioCreateOutput("Out", false);
+	pwm_IO *out2 = pwm_ioCreateOutput("Out2", true);
 	pwm_ioConnect(in, out);
 	pwm_ioConnect(in, out2);
 	pwm_ioConnect(in2, out2);
